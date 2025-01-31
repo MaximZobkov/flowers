331d3c388cd7ed4aace2ff66916127798f8a4cd9
MaximZobkov <vaszweg@mail.ru>
2025-01-31 21:56:25 +0300

    fix problems

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ def init_db():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             description TEXT,
-            price REAL NOT NULL,
+            price INTEGER NOT NULL,
             image_paths TEXT,
             category TEXT NOT NULL
         )
